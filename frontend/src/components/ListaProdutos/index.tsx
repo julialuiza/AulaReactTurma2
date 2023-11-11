ba47720b9232f20d071f440e288ec66a038eb4f2
Douglas <douglas.sm747@gmail.com>
2023-11-11 10:18:36 -0500

    Aula 2

--- frontend/src/components/ListaProdutos/index.tsx
+++ frontend/src/components/ListaProdutos/index.tsx
@@ -1,61 +1,123 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import CustomTable, { TableColumn } from "../Tabela";
+import ConfirmationModal from "../Confirmacao";
+import AttAddProduto from "../AttAddProduto";
+import axios from "axios";
 
-interface IProduto {
+export interface IProduto {
+  id: number;
   nome: string;
   preco: number;
   estoque: number;
 }
 
 export default function ListaProdutos() {
-  const [nameProduct, SetNameProduct] = useState<string>("");
-  const [precoProduct, SetPrecoProduct] = useState<number>(0);
-  const [estoqueProduct, SetEstoqueProduct] = useState<number>(0);
+  const productToDelete = useRef<IProduto>();
+  const productToUpdate = useRef<IProduto>();
 
-  //   const [products, SetProducts] = useState<string[]>([]);
+  const [isModalConfirmationOpen, SetIsModalConfirmationOpen] = useState(false);
+  const [isModalAttAddOpen, SetIsModalAttAddOpenOpen] = useState(false);
+
   const [products, SetProducts] = useState<IProduto[]>([]);
 
-  function AddProductArray() {
-    const jsonProduto: IProduto = {
-      nome: nameProduct,
-      estoque: estoqueProduct,
-      preco: precoProduct,
-    };
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get<IProduto[]>(
+        "http://localhost:3333/v1/produto"
+      );
 
-    SetProducts([...products, jsonProduto]);
+      SetProducts(res.data);
+    }
+
+    fetchData();
+  }, []);
+
+  async function RemoverItemTabela(produtoToDelete: IProduto) {
+    SetProducts(
+      products.filter((produto) => produto.id !== produtoToDelete.id)
+    );
+    
+    await axios.delete(
+      `http://localhost:3333/v1/produto/${produtoToDelete.id}`
+    );
+  }
+
+  const columnsProducts: TableColumn<IProduto>[] = [
+    { head: "ID", acessor: "id" },
+    { head: "Nome", acessor: "nome" },
+    { head: "Estoque", acessor: "estoque" },
+    { head: "Preco", acessor: "preco" },
+    {
+      head: "Remover",
+      isActionButton: true,
+      onActionClick: (obj) => {
+        productToDelete.current = obj;
+        SetIsModalConfirmationOpen(true);
+        //RemoverItemTabela(obj);
+      },
+    },
+    {
+      head: "Editar",
+      isActionButton: true,
+      onActionClick: (obj) => {
+        productToUpdate.current = obj;
+        SetIsModalAttAddOpenOpen(true);
+      },
+    },
+  ];
+
+  function AddProduto(produtoNew: IProduto) {
+    SetProducts([...products, produtoNew]);
+  }
+
+  function AttProduto(produtoAtt: IProduto) {
+    SetProducts(
+      products.map((prod) => {
+        return prod.id === produtoAtt.id ? produtoAtt : prod;
+      })
+    );
   }
 
   return (
     <div>
       <h1>Produtos</h1>
-      <input
-        type="text"
-        value={nameProduct}
-        placeholder="Nome do Produto"
-        onChange={(e) => SetNameProduct(e.target.value)}
-      />
-      <input
-        type="text"
-        value={precoProduct}
-        placeholder="Preco do Produto"
-        onChange={(e) => SetPrecoProduct(parseFloat(e.target.value))}
-      />
-      <input
-        type="text"
-        value={estoqueProduct}
-        placeholder="Estoque do Produto"
-        onChange={(e) => SetEstoqueProduct(parseInt(e.target.value))}
-      />
-      <button onClick={() => AddProductArray()}>Inserir Produto</button>
+      <button
+        onClick={() => {
+          productToUpdate.current = undefined;
+          SetIsModalAttAddOpenOpen(true);
+        }}
+      >
+        Inserir Produto
+      </button>
 
-      {products.map((produto, index) => {
-        return (
-          <div key={index}>
-            <h3>Nome: {produto.nome}</h3>
-            <h4>Preco: {produto.preco}</h4>
-            <h5>Estoque: {produto.estoque}</h5>
-          </div>
-        );
-      })}
+      <CustomTable data={products} columns={columnsProducts} />
+
+      <AttAddProduto
+        isShow={isModalAttAddOpen}
+        onClose={() => {
+          SetIsModalAttAddOpenOpen(false);
+        }}
+        productUpdate={productToUpdate.current}
+        onAddProduct={(produto) => {
+          AddProduto(produto);
+        }}
+        onAttProduct={(produto) => {
+          AttProduto(produto);
+        }}
+      />
+
+      <ConfirmationModal
+        isShow={isModalConfirmationOpen}
+        message="Deseja excluir esse produto?"
+        title="Alerta"
+        onCancel={() => {
+          SetIsModalConfirmationOpen(false);
+        }}
+        onConfirm={() => {
+          RemoverItemTabela(productToDelete.current!);
+          SetIsModalConfirmationOpen(false);
+        }}
+      />
     </div>
   );
 }
